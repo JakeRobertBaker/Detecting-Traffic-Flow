71913382cfb701841ad53b3c630f3d27ba69414e
Jake Baker <jake.baker21@imperial.ac.uk>
2022-09-11 12:40:27 +0100

    linear baseline

---
+++ traffic-flow/Flow-Model-Baseline.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import r2_score
+import datetime
+import calendar
+from collections import defaultdict
+from patsy import dmatrices
+from patsy import dmatrix
+from sklearn.linear_model import LinearRegression
+print('imports done')
+
+
+# Import the traffic data to produce median models
+clean_birmingham_report_df_norm = pd.read_feather('high_quality_traffic_reports_two_year/clean_birmingham_report_df_norm')
+clean_manchester_report_df_norm = pd.read_feather('high_quality_traffic_reports_two_year/clean_manchester_report_df_norm')
+clean_cambridge_report_df_norm = pd.read_feather('high_quality_traffic_reports_two_year/clean_cambridge_report_df_norm')
+clean_thorpe_report_df_norm = pd.read_feather('high_quality_traffic_reports_two_year/clean_thorpe_report_df_norm')
+clean_epping_report_df_norm = pd.read_feather('high_quality_traffic_reports_two_year/clean_epping_report_df_norm')
+clean_bristol_report_df_norm = pd.read_feather('high_quality_traffic_reports_two_year/clean_bristol_report_df_norm')
+
+reports = {"birmingham": clean_birmingham_report_df_norm,
+          "manchester" : clean_manchester_report_df_norm,
+          "cambridge" : clean_cambridge_report_df_norm,
+          "thorpe" : clean_thorpe_report_df_norm,
+          "epping" : clean_epping_report_df_norm,
+          "bristol" : clean_bristol_report_df_norm }
+
+times = {"birmingham": datetime.datetime(2021,6,15,11,14),
+          "manchester" : datetime.datetime(2019,4,19,12,59),
+          "cambridge" : datetime.datetime(2021,11,2,11,29),
+          "thorpe" : datetime.datetime(2022,3,8,11,14),
+          "epping" : datetime.datetime(2022,3,8,11,14),
+          "bristol" : datetime.datetime(2020,8,8,11,14) }
+
+
+# Function that produces the train report
+def get_reports(test_loc):
+    train_report = pd.concat([v for k,v in reports.items() if k != test_loc])
+    return train_report
+
+
+# Function that creates the time factors
+def add_time_data(report):
+    report.loc[:,'hour'] = report.timestamp.dt.hour.astype(str)
+    report.loc[:,'DOW'] =  report.timestamp.dt.day_of_week.astype(str)
+    report.loc[:,'month'] = report.timestamp.dt.month.astype(str)
+    return report
+
+
+def year_str(time):
+    '''
+    Returns two strings representing the first and last day of time's year
+    '''
+    start_time = str(time.year) + "-01-01 00:14:00"
+    end_time = str(time.year) + "-12-31 23:59:00"
+    return start_time, end_time
+
+
+for area in reports.keys():
+    print(area)
+
+    train_report = get_reports(area)
+    start_time, end_time = year_str(times[area])
+    train_report = train_report[train_report.timestamp.between(start_time, end_time)]
+    
+    test_report = pd.DataFrame(pd.date_range(start_time, end_time, freq="15min"))
+    test_report.columns = ['timestamp']
+    print('reports compiled')
+
+    # Add the time factors to the data
+    train_report = add_time_data(train_report)
+    test_report = add_time_data(test_report)
+
+    # Filter down onto the model columns
+    data_train = train_report[['total_volume_normalised', 'hour', 'DOW', 'month']]
+    data_test = test_report[['hour', 'DOW', 'month']]
+
+    # Free up some ram
+    del train_report
+
+    # Make Model Matrix
+    y_train, X_train = dmatrices("total_volume_normalised ~ month + DOW + hour + DOW*hour", data_train)
+    print('train matrix done')
+
+    # Fit model
+    model = LinearRegression()
+    model.fit(X_train,y_train)
+    print('model fitted')
+
+    # Create model matrix for test data
+    X_test = dmatrix("month + DOW + hour + DOW*hour", data_test)
+    print('test matrix done')
+
+    # Make predictions on test data
+    predictions_test = model.predict(X_test)
+    print('test predictions done')
+
+    # Put the predictions in the test report
+    test_report.loc[:,'total_volume_normalised_predictions'] = predictions_test
+
+    # Export result
+    test_report.to_feather(f'linear-{area}')
+    print('exported')
--- traffic-flow/download_high_quality_reports.py
+++ traffic-flow/download_high_quality_reports.py
@@ -9,7 +9,7 @@ with urllib.request.urlopen(url_text) as url:
 sites = data['sites']
 
 
-start_date, end_date = '19032019', '08042022'
+start_date, end_date = '01012019', '08042022'
 argument = sys.argv[-1]
 
 # Go though the area mentioned in arguments and download reports
@@ -18,39 +18,39 @@ if argument in ['birmingham', 'all']:
     max_lat, max_long = 52.50, -1.67
     min_lat, min_long = 52.42, -1.75
     clean_birmingham_report_df_norm, __ = download_clean_pipeline(start_date, end_date, max_lat, max_long, min_lat, min_long)
-    clean_birmingham_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports/clean_birmingham_report_df_norm')
+    clean_birmingham_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports_two_year/clean_birmingham_report_df_norm')
     
 if argument in ['manchester', 'all']:
     print('Querying Manchester')
     max_lat, max_long = 53.51, -2.31
     min_lat, min_long = 53.44, -2.39
     clean_manchester_report_df_norm, __ = download_clean_pipeline(start_date, end_date, max_lat, max_long, min_lat, min_long)
-    clean_manchester_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports/clean_manchester_report_df_norm')
+    clean_manchester_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports_two_year/clean_manchester_report_df_norm')
 
 if argument in ['cambridge', 'all']:
     print('Querying Cambridge')
     max_lat, max_long = 52.25, 0.11
     min_lat, min_long = 52.19, 0.02
     clean_cambridge_report_df_norm, __ = download_clean_pipeline(start_date, end_date, max_lat, max_long, min_lat, min_long)
-    clean_cambridge_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports/clean_cambridge_report_df_norm')
+    clean_cambridge_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports_two_year/clean_cambridge_report_df_norm')
     
 if argument in ['thorpe', 'all']:
     print('Querying Thorpe')
     max_lat, max_long = 51.43, -0.50
     min_lat, min_long = 51.38, -0.57
     clean_thorpe_report_df_norm, __ = download_clean_pipeline(start_date, end_date, max_lat, max_long, min_lat, min_long)
-    clean_thorpe_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports/clean_thorpe_report_df_norm')
+    clean_thorpe_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports_two_year/clean_thorpe_report_df_norm')
     
 if argument in ['epping', 'all']:
     print('Querying Epping')
     max_lat, max_long = 51.72, 0.15
     min_lat, min_long = 51.62, 0.09
     clean_epping_report_df_norm, __ = download_clean_pipeline(start_date, end_date, max_lat, max_long, min_lat, min_long)
-    clean_epping_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports/clean_epping_report_df_norm')
+    clean_epping_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports_two_year/clean_epping_report_df_norm')
 
 if argument in ['bristol', 'all']:
     print('Querying Bristol')
     max_lat, max_long = 51.60, -2.52
     min_lat, min_long = 51.52, -2.59
     clean_bristol_report_df_norm, __ = download_clean_pipeline(start_date, end_date, max_lat, max_long, min_lat, min_long)
-    clean_bristol_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports/clean_bristol_report_df_norm')
+    clean_bristol_report_df_norm.reset_index(drop=True).to_feather('high_quality_traffic_reports_two_year/clean_bristol_report_df_norm')
